93c106d249c99ce84f0995ecb88de8c78ed76700
danielxz331 <Tu direccin de correo electrnico>
2023-05-29 23:53:38 -0500

    Validacion codigos distintos

--- clienteh.c
+++ clienteh.c
@@ -15,26 +15,30 @@ void solicitarNombre(char *nombre);
 void solicitarNumero(char *nombre);
 int validarDigitos(const char *numero);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int sock;
     char com[SIZE];
     struct sockaddr_in adr;
     struct hostent *hp, *gethostbyname();
-    int n;
+    int n, error;
     char recvline[MAXLINE];
     char nombre[MAXLINE];
     char numero[MAXLINE];
-    if (argc != 3) {
+    if (argc != 3)
+    {
         fprintf(stderr, "Uso: %s <host> <port>\n", argv[0]);
         exit(1);
     }
 
-    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+    {
         perror("Imposible creacion del socket");
         exit(2);
     }
 
-    if ((hp = gethostbyname(argv[1])) == NULL) {
+    if ((hp = gethostbyname(argv[1])) == NULL)
+    {
         perror("Error: Nombre de la maquina desconocido");
         exit(3);
     }
@@ -44,11 +48,12 @@ int main(int argc, char *argv[]) {
     adr.sin_port = htons(atoi(argv[2]));
     bcopy(hp->h_addr, &adr.sin_addr, hp->h_length);
 
-    if (connect(sock, (struct sockaddr *)&adr, sizeof(adr)) == -1) {
+    if (connect(sock, (struct sockaddr *)&adr, sizeof(adr)) == -1)
+    {
         perror("connect");
         exit(4);
     }
-    //recibir bienvenida
+    // recibir bienvenida
     n = read(sock, recvline, MAXLINE - 1);
     recvline[n] = '\0';
     printf("%s", recvline);
@@ -56,25 +61,38 @@ int main(int argc, char *argv[]) {
     solicitarNombre(nombre);
     write(sock, nombre, strlen(nombre));
 
-    solicitarNumero(numero);
-    write(sock, numero, strlen(numero));
-    
-    n = read(sock, recvline, MAXLINE - 1);
-    recvline[n] = '\0';
-    printf("%s", recvline);
+    do
+    {
+        solicitarNumero(numero);
+        write(sock, numero, strlen(numero));
+
+        n = read(sock, recvline, MAXLINE - 1);
+        recvline[n] = '\0';
+        error = atoi(recvline);
+        
+        if (error == 0)
+        {
+            printf("Este numero ya fue ingresado \n");
+        }
+        
+
+    } while (error == 0);
 
     str_echo(sock);
 
     return 0;
 }
 
-void str_echo(int sock) {
+void str_echo(int sock)
+{
     char sendline[MAXLINE], recvline[MAXLINE];
     int n;
-    while (1) {
+    while (1)
+    {
         fgets(sendline, MAXLINE, stdin);
         write(sock, sendline, strlen(sendline));
-        if (read(sock, recvline, MAXLINE) == 0) {
+        if (read(sock, recvline, MAXLINE) == 0)
+        {
             fprintf(stderr, "Servidor terminado prematuramente\n");
             exit(5);
         }
@@ -82,62 +100,80 @@ void str_echo(int sock) {
     }
 }
 
-void solicitarNombre(char *nombre) {
+void solicitarNombre(char *nombre)
+{
     printf("Por favor, ingresa tu nombre: ");
     fgets(nombre, MAXLINE, stdin);
     nombre[strcspn(nombre, "\n")] = '\0';
 }
 
-
-int validarDigitos(const char *numero) {
+int validarDigitos(const char *numero)
+{
     int digitos[10] = {0};
     int longitud = strlen(numero);
-    if (longitud == 4) {
+    if (longitud == 4)
+    {
         // Verificar que todos los caracteres sean dígitos
-        for (int i = 0; i < longitud; i++) {
-            if (numero[i] < '0' || numero[i] > '9') {
-                return 3;  // Carácter no válido encontrado
+        for (int i = 0; i < longitud; i++)
+        {
+            if (numero[i] < '0' || numero[i] > '9')
+            {
+                return 3; // Carácter no válido encontrado
             }
         }
-        
+
         // Verificar que el primer dígito sea diferente de 0
-        if (numero[0] == '0') {
-            return 4;  // El primer dígito es 0
+        if (numero[0] == '0')
+        {
+            return 4; // El primer dígito es 0
         }
-        
+
         // Contar los dígitos y verificar si hay repetidos
-        for (int i = 0; i < longitud; i++) {
+        for (int i = 0; i < longitud; i++)
+        {
             int digito = numero[i] - '0'; // Convertir a int
-            digitos[digito]++;  // Incrementar el contador del dígito
-            if (digitos[digito] > 1) {
-                return 1;  // Dígito repetido encontrado
+            digitos[digito]++;            // Incrementar el contador del dígito
+            if (digitos[digito] > 1)
+            {
+                return 1; // Dígito repetido encontrado
             }
         }
-        
-        return 0;  // No se encontraron dígitos repetidos
-    } else {
+
+        return 0; // No se encontraron dígitos repetidos
+    }
+    else
+    {
         return 2; // Cantidad de dígitos inválida
     }
 }
 
-void solicitarNumero(char *numero) {
+void solicitarNumero(char *numero)
+{
     int repeticion = 0;
-    do {
+    do
+    {
         printf("Ingresa el numero: ");
         fgets(numero, MAXLINE, stdin);
         numero[strcspn(numero, "\n")] = '\0';
         repeticion = validarDigitos(numero);
-        if (repeticion) { //el return de digitos es 1 o 2
-            if (repeticion == 1){
+        if (repeticion)
+        { // el return de digitos es 1 o 2
+            if (repeticion == 1)
+            {
                 printf("Error: Los digitos no deben repetirse. Intenta nuevamente.\n");
             }
-            else if (repeticion==2){
+            else if (repeticion == 2)
+            {
                 printf("Error: Cantidad de dígitos inválida. Intenta nuevamente.\n");
-            }else if(repeticion==3){
+            }
+            else if (repeticion == 3)
+            {
                 printf("Error: Se ingresaron caracteres, deben ser digitos. Intenta nuevamente.\n");
-            }else{
+            }
+            else
+            {
                 printf("Error: Primer digito debe ser diferente de 0. Intenta nuevamente.\n");
             }
         }
-    } while (repeticion);//sale cuando el return es 0 
+    } while (repeticion); // sale cuando el return es 0
 }
--- servidor.c
+++ servidor.c
@@ -11,6 +11,7 @@
 #include <netdb.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 #define PORT 2222
 #define MAXLINE 4096
@@ -22,8 +23,10 @@
 pthread_mutex_t semaforo, semaforo2;
 
 int hilos = 0;
+int validar = 1;
 
-struct Jugador {
+struct Jugador
+{
     char nombre[MAX_NOMBRE];
     char codigo[MAX_CODIGO];
 };
@@ -32,12 +35,14 @@ struct Jugador jugadores[MAX_JUGADORES];
 
 int sock_servicio[10];
 
-int crearsocket(int *port, int type) {
+int crearsocket(int *port, int type)
+{
     int sockfd;
     struct sockaddr_in adr;
     int longitud;
 
-    if ((sockfd = socket(PF_INET, type, 0)) == -1) {
+    if ((sockfd = socket(PF_INET, type, 0)) == -1)
+    {
         perror("Error: Imposible crear socket");
         exit(2);
     }
@@ -47,14 +52,16 @@ int crearsocket(int *port, int type) {
     adr.sin_addr.s_addr = htonl(INADDR_ANY);
     adr.sin_family = PF_INET;
 
-    if (bind(sockfd, (struct sockaddr *)&adr, sizeof(adr)) == -1) {
+    if (bind(sockfd, (struct sockaddr *)&adr, sizeof(adr)) == -1)
+    {
         perror("Error: bind");
         exit(3);
     }
 
     longitud = sizeof(adr);
 
-    if (getsockname(sockfd, (struct sockaddr *)&adr, &longitud)) {
+    if (getsockname(sockfd, (struct sockaddr *)&adr, &longitud))
+    {
         perror("Error: Obtencion del nombre del sock");
         exit(4);
     }
@@ -63,7 +70,8 @@ int crearsocket(int *port, int type) {
     return sockfd;
 }
 
-void sigchld() {
+void sigchld()
+{
     pid_t pid;
     int stat;
 
@@ -72,7 +80,28 @@ void sigchld() {
     return;
 }
 
-void *servicio(void *sock) {
+void validarRepetido(const char *codigo)
+{
+    int i;
+    int codigoJugador;
+    int codigoEntrante = atoi(codigo);
+
+    for (i = 0; i < MAX_JUGADORES; i++)
+    {
+        codigoJugador = atoi(jugadores[i].codigo);
+
+        if (codigoJugador == codigoEntrante)
+        {
+            validar = 0;
+            return;
+        }
+    }
+    validar = 1;
+    return;
+}
+
+void *servicio(void *sock)
+{
     pthread_mutex_lock(&semaforo);
     int client_sock = *(int *)sock;
     ssize_t n, m;
@@ -87,17 +116,43 @@ void *servicio(void *sock) {
     line[n] = '\0';
     printf("Nombre del cliente: %s\n", line);
 
-    strncpy(jugadores[hilos-1].nombre, line, MAX_NOMBRE);  // Guardar nombre del jugador
+    strncpy(jugadores[hilos - 1].nombre, line, MAX_NOMBRE); // Guardar nombre del jugador
 
     // Leer código del cliente
-    m = read(client_sock, line, MAXLINE - 1);
-    line[m] = '\0';
-    printf("Código del cliente: %s\n", line);
+    if (hilos == 1)
+    {
+        m = read(client_sock, line, MAXLINE - 1);
+        line[m] = '\0';
+        const char *mensaje_Error = "1";
+        write(client_sock, mensaje_Error, strlen(mensaje_Error));
+        printf("Código del cliente: %s\n", line);
+        strncpy(jugadores[hilos - 1].codigo, line, MAX_CODIGO); // Guardar código del jugador
+    }
+    else
+    {
+        do
+        {
 
-    strncpy(jugadores[hilos-1].codigo, line, MAX_CODIGO);  // Guardar código del jugador
+            m = read(client_sock, line, MAXLINE - 1);
+            line[m] = '\0';
+            validarRepetido(line);
+
+            if (validar == 0)
+            {
+                const char *mensaje_Error = "0";
+                write(client_sock, mensaje_Error, strlen(mensaje_Error));
+            }
+
+        } while (validar == 0);
+        const char *mensaje_Error = "1";
+        write(client_sock, mensaje_Error, strlen(mensaje_Error));
+        printf("Código del cliente: %s\n", line);
+        strncpy(jugadores[hilos - 1].codigo, line, MAX_CODIGO); // Guardar código del jugador
+    }
 
     // imprimir arreglo
-    for(int i=0; i<4; i++){
+    for (int i = 0; i < 4; i++)
+    {
         printf("%s", jugadores[i].nombre);
         printf("|");
         printf("%s", jugadores[i].codigo);
@@ -106,7 +161,8 @@ void *servicio(void *sock) {
     pthread_mutex_unlock(&semaforo);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int sock_escucha;
     struct sockaddr_in adr;
     int lgadr = sizeof(adr);
@@ -116,14 +172,16 @@ int main(int argc, char *argv[]) {
     int max_clientes = 4;
     int finalizar = 0;
 
-    if (argc != 2) {
+    if (argc != 2)
+    {
         fprintf(stderr, "Uso: %s [port]\n", argv[0]);
         exit(1);
     }
 
     port = atoi(argv[1]);
 
-    if ((sock_escucha = crearsocket(&port, SOCK_STREAM)) == -1) {
+    if ((sock_escucha = crearsocket(&port, SOCK_STREAM)) == -1)
+    {
         fprintf(stderr, "Error: No se pudo crear/conectar socket\n");
         exit(2);
     }
@@ -134,33 +192,38 @@ int main(int argc, char *argv[]) {
 
     fprintf(stdout, "Inicio servidor en el puerto %d\n", port);
 
-    while (TRUE) {
+    while (TRUE)
+    {
         if (finalizar == 1)
             break;
 
         lgadr = sizeof(adr);
         sock_servicio[hilos] = accept(sock_escucha, (struct sockaddr *)&adr, &lgadr);
 
-        if (clientes_conectados >= max_clientes) {
+        if (clientes_conectados >= max_clientes)
+        {
             const char *mensaje = "Cantidad de jugadores máxima conectada\n";
             write(sock_servicio[hilos], mensaje, strlen(mensaje));
             close(sock_servicio[hilos]);
-        } else {
+        }
+        else
+        {
             fprintf(stdout, "Servicio aceptado: %d\n", hilos);
-            pthread_create(&t1[hilos], NULL, servicio, &sock_servicio[hilos]);     
-            pthread_mutex_lock(&semaforo);      
+            pthread_create(&t1[hilos], NULL, servicio, &sock_servicio[hilos]);
+            pthread_mutex_lock(&semaforo);
             hilos++;
             pthread_mutex_unlock(&semaforo);
             clientes_conectados++;
         }
 
-        if (clientes_conectados >= max_clientes) {
+        if (clientes_conectados >= max_clientes)
+        {
             const char *mensaje = "Cantidad de jugadores máxima conectada\n";
             write(sock_servicio[hilos], mensaje, strlen(mensaje));
         }
     }
 
-    close(sock_escucha);  // Cerrar el socket de escucha después del bucle
+    close(sock_escucha); // Cerrar el socket de escucha después del bucle
 
     return 0;
 }
